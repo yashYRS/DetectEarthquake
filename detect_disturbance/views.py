27a70be262609eca02d8b4dc15b25c81791b03d1
yashYRS <yash@glib.ai>
2021-09-15 18:32:23 +0530

    Different buttons have the ability to perform different functions now

--- detect_disturbance/views.py
+++ detect_disturbance/views.py
@@ -4,18 +4,36 @@ from django.shortcuts import render
 from .models import Videos
 
 
+def save_uploaded_video(post_request):
+    curr_title = post_request['title']
+    curr_video = post_request['video']
+    if curr_video:
+        content = Videos(title=curr_title,
+                         video=curr_video)
+        content.save()
+    else:
+        # Enter Message box, showing that no video has been chosen
+        pass
+
+
 def index(request):
     context = {}
     print(" I monitored something ")
     if request.method == 'POST':
+        if 'bg_video' in request.POST:
+            print('Disturbance analyzed')
+            save_uploaded_video(request.POST)
+            # Call function to analyze video
 
-        title = request.POST['title']
-        video = request.POST['video']
+        elif 'animal_video' in request.POST:
+            print('Entry exit analyzed')
+            save_uploaded_video(request.POST)
+            # Call function to monitor entry and exit in video
 
-        content = Videos(title=title, video=video)
-        content.save()
-        print(" I uploaded something ")
-        render(request, 'monitor.html', context)
+        elif 'animal_webcam' in request.POST:
+            print('Web cam to be opened')
+            # Call function to open webcam and analyze entry and exit
+
     return render(request, 'monitor.html', context)
 
 
